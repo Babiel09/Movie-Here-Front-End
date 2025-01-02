b1432382aaa11e55f8e7eff49fc66f06929855cd
GuilhermeNunes <digobros13@gmail.com>
2025-01-02 19:04:33 -0300

    style: updated tags.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,35 @@
 import { Clapperboard, House, User } from 'lucide-react';
-
+import Link from "next/link"
 
 export function Header() {
     return (
-        <>
-            <header className='bg-blue-500 text-white p-4'>
-                <div className='flex justify-center text-center mt-4'>
-                    <Clapperboard size={70} className=""/>
-                    <h3 className='text-2xl pt-4 pl-2 font-semibold md:pl-5'>
-                        Bem vindo ao Movie Here
-                    </h3>
-                </div>
+        <header className='bg-blue-500 text-white p-4'>
+            <div className='flex justify-center text-center mt-4'>
+                <Clapperboard size={70} className=""/>
+                <h3 className='text-2xl pt-4 pl-2 font-semibold md:pl-5'>
+                    Movie Here
+                </h3>
+            </div>
 
-                <div className='flex justify-end pt-4'>
-                    
-                    <a href="#" className='pl-5'>
-                    <span className='hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'>   
-                            Home
-                        </span>
-                    </a>
-                    
-                    <a href="#" className='pl-5'>
-                        <span className='hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'>   
-                            Movies
-                        </span>
-                    </a>
-                    <a href="#" className='pr-7 pl-5'>
-                        <span className='hover:bg-blue-600 text-white font-bold py-2 px-2 rounded hover:scale-105 duration-200'>   
-                            Login
-                        </span>
-                    </a>
-                </div> 
+            <div className='flex justify-end pt-4'>
                 
-            </header>
-        </>
+                <Link href="#" className='pl-5'>
+                    <span className='hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'>   
+                        Home
+                    </span>
+                </Link>
+                
+                <Link href="#" className='pl-5'>
+                    <span className='hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'>   
+                        Movies
+                    </span>
+                </Link>
+                <Link href="#" className='pr-7 pl-5'>
+                    <span className='hover:bg-blue-600 text-white font-bold py-2 px-2 rounded hover:scale-105 duration-200'>   
+                        Login
+                    </span>
+                </Link>
+            </div>          
+        </header>
     )
 }
