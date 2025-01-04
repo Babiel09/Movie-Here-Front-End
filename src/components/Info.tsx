5fc1cd85d4939ec4a12949618c5ef4e973fa7ac7
GuilhermeNunes <digobros13@gmail.com>
2025-01-03 21:56:55 -0300

    added: info about movie here

---
+++ src/components/Info.tsx
@@ -0,0 +1,15 @@
+
+export function Info() {
+    return (
+        <section>
+            <div className="mt-4 text-center p-2">
+                <h4 className="text-3xl mb-3">
+                    Bem vindo ao Movie Here
+                </h4>
+                <p>
+                    Aqui no movie here, você poderá encontrar e assistir os melhores filmes!
+                </p>
+            </div>
+        </section>
+    )
+}
