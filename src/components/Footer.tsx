e91eb32925545e19b52e72797dbdb334eeea9c45
GuilhermeNunes <digobros13@gmail.com>
2025-01-08 21:06:04 -0300

    added: pictures and description abour movie here.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,35 @@
-
+import { Clapperboard } from 'lucide-react';
+import Image from 'next/image';
+import picgabriel from '../../assets/picgabriel.png';
+import picguilherme from '../../assets/picguilherme.png';
 
 export function Footer() {
     return (
-        <footer className="fixed bottom-0 left-0 bg-blue-500 w-screen h-10 p-10">
-            
+        <footer className="fixed bottom-0 left-0 bg-blue-500 w-screen p-3">
+            <div>
+                
+                
+                    <div className='flex justify-center'>
+                        <Image
+                        src={picgabriel}
+                        alt='Gabriel. Github: "babiel09".'
+                        className='size-28 rounded-lg p-2'/>
+
+                        <Image
+                        src={picguilherme}
+                        alt='Gabriel. Github: Nu S "nunesIug".'
+                        className='size-28 rounded-xl p-2'/>
+                    <div className='flex justify-end'>
+                        <Clapperboard size={90} className='md:size-30'/>
+                    </div>
+                </div>
+                <p className='p-2'>
+                    Movie Here é um website de streaming de filmes antigos até aos novos. <br />
+                    Todos os filmes que tiver interesse em assistir, busque aqui que você achará.
+                </p>
+                
+                
+            </div>
         </footer>
     )
 }
