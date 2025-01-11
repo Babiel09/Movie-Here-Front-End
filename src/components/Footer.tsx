88537fc5b1ba1abbf80688cd5c89ad294d65cb2c
GuilhermeNunes <digobros13@gmail.com>
2025-01-11 11:53:08 -0300

    modified: indentation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,30 +5,24 @@ import picguilherme from '../../assets/picguilherme.png';
 
 export function Footer() {
     return (
-        <footer className="fixed bottom-0 left-0 bg-blue-500 w-screen p-3">
+        <footer className="fixed bottom-0 left-0 bg-blue-500 w-screen p-3">  
             <div>
-                
-                
-                    <div className='flex justify-center'>
-                        <Image
-                        src={picgabriel}
-                        alt='Gabriel. Github: "babiel09".'
-                        className='size-28 rounded-lg p-2'/>
+                <div className='flex justify-end'>
+                    <Image
+                    src={picgabriel}
+                    alt='Gabriel. Github: "babiel09".'
+                    className='size-28 rounded-lg p-2'/>
 
-                        <Image
-                        src={picguilherme}
-                        alt='Gabriel. Github: Nu S "nunesIug".'
-                        className='size-28 rounded-xl p-2'/>
-                    <div className='flex justify-end'>
-                        <Clapperboard size={90} className='md:size-30'/>
-                    </div>
+                    <Image
+                    src={picguilherme}
+                    alt='Gabriel. Github: Nu S "nunesIug".'
+                    className='size-28 rounded-xl p-2'/>
+                    <Clapperboard size={90} className='md:size-30'/>
                 </div>
                 <p className='p-2'>
                     Movie Here é um website de streaming de filmes antigos até aos novos. <br />
                     Todos os filmes que tiver interesse em assistir, busque aqui que você achará.
                 </p>
-                
-                
             </div>
         </footer>
     )
