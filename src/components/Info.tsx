6d1d848c8e6d6592fd25a3b2fac39e5ed71cd33d
GuilhermeNunes <digobros13@gmail.com>
2025-01-11 11:53:25 -0300

    feat: added get started button

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -6,9 +6,11 @@ export function Info() {
                 <h4 className="text-3xl mb-3">
                     Bem vindo ao Movie Here
                 </h4>
-                <p>
+                <p className="mb-16">
                     Aqui no movie here, você poderá encontrar e assistir os melhores filmes!
-                </p>
+                </p>  
+
+                <a href="#" className="rounded-md bg-blue-600 py-2 px-8 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-blue-700 focus:shadow-none active:bg-blue-700 hover:bg-blue-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">Iniciar</a>
             </div>
         </section>
     )
